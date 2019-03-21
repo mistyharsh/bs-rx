c09d06f1b41a367803b77fba2dec82c0a9fade7b
Harshal Patil <harshal.rp@gmail.com>
2019-03-21 22:47:55 +0530

    Unit tests for concat, defer and deferArray

--- test/Rx_test.bs.js
+++ test/Rx_test.bs.js
@@ -2,7 +2,9 @@
 
 import * as Jest from "@glennsl/bs-jest/src/jest.js";
 import * as Rxjs from "rxjs";
+import * as $$Array from "bs-platform/lib/es6/array.js";
 import * as Curry from "bs-platform/lib/es6/curry.js";
+import * as Operators from "rxjs/operators";
 
 var oneObs = Rxjs.Observable.create((function (obs) {
         obs.next(10);
@@ -18,47 +20,82 @@ var twoObs = Rxjs.Observable.create((function (obs) {
       }));
 
 Jest.describe("Expect", (function (param) {
-        return Jest.test("Sample test", (function (param) {
-                      return Jest.Expect[/* toBe */2](3, Jest.Expect[/* expect */0](3));
+        Jest.test("Sample test", (function (param) {
+                return Jest.Expect[/* toBe */2](3, Jest.Expect[/* expect */0](3));
+              }));
+        return Jest.testAsync("Rx.create - Create observable using static method", undefined, (function (finish) {
+                      var obs = Rxjs.Observable.create((function (obs) {
+                              obs.next(20);
+                              obs.complete();
+                              return /* () */0;
+                            }));
+                      obs.subscribe((function (value) {
+                              return Curry._1(finish, Jest.Expect[/* toBe */2](20, Jest.Expect[/* expect */0](value)));
+                            }));
+                      return /* () */0;
                     }));
       }));
 
-Jest.testAsync("Rx.create - Create observable using static method", undefined, (function (finish) {
-        var obs = Rxjs.Observable.create((function (obs) {
-                obs.next(20);
-                obs.complete();
+Jest.describe("combineLatest", (function (param) {
+        Jest.testAsync("combineLatest2", undefined, (function (finish) {
+                var testObs = Rxjs.combineLatest(twoObs, oneObs);
+                testObs.subscribe((function (value) {
+                        return Curry._1(finish, Jest.Expect[/* toEqual */12](/* tuple */[
+                                        200,
+                                        10
+                                      ], Jest.Expect[/* expect */0](value)));
+                      }));
                 return /* () */0;
               }));
-        obs.subscribe((function (value) {
-                return Curry._1(finish, Jest.Expect[/* toBe */2](20, Jest.Expect[/* expect */0](value)));
-              }));
-        return /* () */0;
+        return Jest.testAsync("combineLatest6", undefined, (function (finish) {
+                      var testObs = Rxjs.combineLatest(oneObs, oneObs, oneObs, oneObs, oneObs, oneObs);
+                      testObs.subscribe((function (value) {
+                              return Curry._1(finish, Jest.Expect[/* toEqual */12](/* tuple */[
+                                              10,
+                                              10,
+                                              10,
+                                              10,
+                                              10,
+                                              10
+                                            ], Jest.Expect[/* expect */0](value)));
+                            }));
+                      return /* () */0;
+                    }));
       }));
 
-Jest.testAsync("combineLatest2", undefined, (function (finish) {
-        var testObs = Rxjs.combineLatest(twoObs, oneObs);
-        testObs.subscribe((function (value) {
-                return Curry._1(finish, Jest.Expect[/* toEqual */12](/* tuple */[
-                                200,
-                                10
-                              ], Jest.Expect[/* expect */0](value)));
+Jest.describe("Static operators", (function (param) {
+        Jest.testAsync("concat", undefined, (function (finish) {
+                var testObs = Curry._1(Operators.reduce((function (acc, n, _i) {
+                            return $$Array.append(acc, /* array */[n]);
+                          }), /* array */[]), Rxjs.concat(oneObs, twoObs, oneObs));
+                testObs.subscribe((function (value) {
+                        return Curry._1(finish, Jest.Expect[/* toEqual */12](/* array */[
+                                        10,
+                                        100,
+                                        200,
+                                        10
+                                      ], Jest.Expect[/* expect */0](value)));
+                      }));
+                return /* () */0;
               }));
-        return /* () */0;
-      }));
-
-Jest.testAsync("combineLatest6", undefined, (function (finish) {
-        var testObs = Rxjs.combineLatest(oneObs, oneObs, oneObs, oneObs, oneObs, oneObs);
-        testObs.subscribe((function (value) {
-                return Curry._1(finish, Jest.Expect[/* toEqual */12](/* tuple */[
-                                10,
-                                10,
-                                10,
-                                10,
-                                10,
-                                10
-                              ], Jest.Expect[/* expect */0](value)));
+        Jest.testAsync("defer", 100, (function (finish) {
+                var testObs = Rxjs.defer((function () {
+                        return oneObs;
+                      }));
+                testObs.subscribe((function (value) {
+                        return Curry._1(finish, Jest.Expect[/* toBe */2](10, Jest.Expect[/* expect */0](value)));
+                      }));
+                return /* () */0;
               }));
-        return /* () */0;
+        return Jest.testAsync("deferArray", 100, (function (finish) {
+                      var testObs = Rxjs.defer((function (param) {
+                              return /* array */[20];
+                            }));
+                      testObs.subscribe((function (value) {
+                              return Curry._1(finish, Jest.Expect[/* toBe */2](20, Jest.Expect[/* expect */0](value)));
+                            }));
+                      return /* () */0;
+                    }));
       }));
 
 export {
--- test/Rx_test.ml
+++ test/Rx_test.ml
@@ -1,6 +1,7 @@
 open Jest
-open Expect
+
 open Rx
+open Operators
 
 let oneObs = create (fun obs ->
   next obs 10;
@@ -14,28 +15,62 @@ let twoObs = create (fun obs ->
 )
 
 let _ = describe "Expect" begin fun () ->
-  test "Sample test" (fun ()  -> expect (1 + 2) |> (toBe 3))
+  let open Expect in
+
+  test "Sample test" (fun ()  -> expect (1 + 2) |> (toBe 3));
+
+  testAsync "Rx.create - Create observable using static method" begin fun finish  ->
+    let obs = create (fun obs ->
+      next obs 20;
+      complete obs
+    ) in
+
+    subscribe obs (fun value -> expect value |> toBe 20 |> finish) |> ignore
+  end;
 end
 
-let _ = testAsync "Rx.create - Create observable using static method" begin fun finish  ->
-  let obs = create (fun obs ->
-    next obs 20;
-    complete obs
-  ) in
+let _ = describe "combineLatest" begin fun () ->
+  let open Expect in
 
-  subscribe obs (fun value -> expect value |> toBe 20 |> finish) |> ignore
+  testAsync "combineLatest2" begin fun finish ->
+    let testObs = combineLatest2 twoObs oneObs in
+
+    subscribe testObs (fun value ->
+      expect value |> toEqual (200, 10) |> finish) |> ignore
+  end;
+
+  testAsync "combineLatest6" begin fun finish ->
+    let testObs = combineLatest6 oneObs oneObs oneObs oneObs oneObs oneObs in
+
+    subscribe testObs (fun value ->
+      expect value |> toEqual (10, 10, 10, 10, 10, 10) |> finish) |> ignore
+  end;
+
 end
 
-let _ = testAsync "combineLatest2" begin fun finish ->
-  let testObs = combineLatest2 twoObs oneObs in
+let _ = describe "Static operators" begin fun () ->
+  let open Expect in
 
-  subscribe testObs (fun value ->
-    expect value |> toEqual (200, 10) |> finish) |> ignore
-end
+  testAsync "concat" begin fun finish ->
+    let testObs = concat [| oneObs; twoObs; oneObs |]
+      |> reduce (fun acc n _i -> Array.append acc [| n |]) [||] in
 
-let _ = testAsync "combineLatest6" begin fun finish ->
-  let testObs = combineLatest6 oneObs oneObs oneObs oneObs oneObs oneObs in
+    subscribe testObs (fun value ->
+      expect value |> toEqual [| 10; 100; 200; 10 |] |> finish) |> ignore
+  end;
 
-  subscribe testObs (fun value ->
-    expect value |> toEqual (10, 10, 10, 10, 10, 10) |> finish) |> ignore
+  testAsync "defer" ~timeout:100 begin fun finish ->
+    let testObs = defer (fun () -> oneObs) in
+
+    subscribe testObs (fun value ->
+      expect value |> toBe 10 |> finish) |> ignore
+  end;
+
+  testAsync "deferArray" ~timeout:100 begin fun finish ->
+    let testObs = deferArray (fun () -> [| 20 |]) in
+
+    subscribe testObs (fun value ->
+      expect value |> toBe 20 |> finish) |> ignore
+  end;
+
 end
